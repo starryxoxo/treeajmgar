d6ad2f34afd4632883365c43f2358e33f04b48d6
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-04-30 20:35:40 +0800

    Update lastPage.js

--- src/helpers/lastPage.js
+++ src/helpers/lastPage.js
@@ -1,4 +1,4 @@
-window.addEventListener("beforeunload", () => {
+window.addEventListener("pagehide", () => {
   localStorage.setItem("lastPage", window.location.href);
   localStorage.setItem("scrollPos", window.scrollY);
 });
