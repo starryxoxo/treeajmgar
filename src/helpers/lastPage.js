cf8bf0e4e57c3500ca8ffb4bdc1a4dcb79c3ba7e
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-04-30 20:42:14 +0800

    Update lastPage.js

--- src/helpers/lastPage.js
+++ src/helpers/lastPage.js
@@ -1,4 +1,11 @@
-window.addEventListener("pagehide", () => {
+function saveProgress() {
   localStorage.setItem("lastPage", window.location.href);
   localStorage.setItem("scrollPos", window.scrollY);
+}
+
+window.addEventListener("beforeunload", saveProgress);
+window.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    saveProgress();
+  }
 });
