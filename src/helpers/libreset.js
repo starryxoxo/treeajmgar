65f085b83559b6781ce98942f83ab2ba58f2dedc
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-05-04 17:58:44 +0800

    Delete src/helpers/libreset.js

--- src/helpers/libreset.js
+++
@@ -1,9 +0,0 @@
-// Function to reset the library (clear all saved books)
-  function resetLibrary() {
-    if (confirm("This will remove all books from your reading list. Continue?")) {
-      localStorage.removeItem("bookLibrary");
-      renderLibrary();
-    }
-  }
-
-document.getElementById("reset-collection").addEventListener("click", resetLibrary);
