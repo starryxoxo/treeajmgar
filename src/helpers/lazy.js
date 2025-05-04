8fdbb010648e81b08537855d3201c815a737884b
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-05-04 17:52:08 +0800

    Delete src/helpers/lazy.js

--- src/helpers/lazy.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll("img:not([loading])");
-    images.forEach(img => {
-      img.setAttribute("loading", "lazy");
-    });
-  });
