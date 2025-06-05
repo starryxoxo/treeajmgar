3fa3fc1d3381275f549b89a44bcb5a28fc455e5b
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-06-05 09:03:42 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -524,6 +524,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
+
+eleventyConfig.addPassthroughCopy("books.json");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
