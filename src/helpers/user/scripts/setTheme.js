4f664498f60397aa48d134424568733ac9a48b68
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-06-06 18:12:08 +0800

    Update setTheme.js

--- src/helpers/user/scripts/setTheme.js
+++ src/helpers/user/scripts/setTheme.js
@@ -1,5 +1,5 @@
 function setTheme(theme) {
-  document.body.classList.remove('theme-light', 'theme-paper', 'theme-green');
+  document.body.classList.remove('theme-light', 'theme-paper', 'theme-green', 'theme-purple', 'd-theme-paper', 'd-theme-green', 'd-theme-purple');
   if (theme) {
     document.body.classList.add('theme-' + theme);
     localStorage.setItem('theme', theme);
@@ -23,4 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('theme-light').onclick = () => setTheme('light');
 document.getElementById('theme-paper').onclick = () => setTheme('paper');
 document.getElementById('theme-green').onclick = () => setTheme('green');
+document.getElementById('theme-purple').onclick = () => setTheme('purple');
+document.getElementById('d-theme-paper').onclick = () => setTheme('dark paper');
+document.getElementById('d-theme-green').onclick = () => setTheme('dark green');
+document.getElementById('d-theme-purple').onclick = () => setTheme('dark purple');
 document.getElementById('theme-default').onclick = () => setTheme(null);
