03dd33ce49cd69bc5fff4412819b4dec6627397c
starryxoxo <112181939+starryxoxo@users.noreply.github.com>
2025-06-22 11:17:23 +0800

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -6,7 +6,7 @@ function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
  
-    eleventyConfig.addPassthroughCopy ("sw.js");
+eleventyConfig.addPassthroughCopy ("sw.js");
 eleventyConfig.addPassthroughCopy("src/site/books.json");
   eleventyConfig.addPassthroughCopy("app");
   eleventyConfig.addPassthroughCopy("manifest.webmanifest");
